973c34324c3f09e3c8fe6406dc9ee06297384ed6
Gabriel <gamer226+github@gmail.com>
2023-08-15 14:41:33 -0400

    Squashed commit of the following:
    commit 892b5338a29b3fe9c84a6fcca87db71ba0a36f3f
    Author: Gabriel <gamer226+github@gmail.com>
    Date:   Tue Aug 15 14:41:09 2023 -0400
    
        Prevent page from refreshing by submitting a form
        Got rid of a javascript error
        Reverse text both from within and outside buttons
        Clicking a new button reverses text in the buttons
        Broke out Person class, added another starting row
        Added initial entry and adjusted button size.
        Turned text field into row with buttons and icons
        Add button that grows a list of text fields.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 const config = {
-  apiPrefix: '/dev'
+  apiPrefix: "/dev",
 };
 
 class App extends React.Component {
@@ -10,9 +10,27 @@ class App extends React.Component {
     this.state = {
       info: {
         application: "",
-        version: ""
+        version: "",
       },
       config: config,
+      clicks: [this.newClickData()],
+    };
+    this.state.clicks = [...this.state.clicks, this.newClickData()]; // Add another row
+    this.addPersonRow = this.addPersonRow.bind(this);
+    this.reverseRandomPersonText = this.reverseRandomPersonText.bind(this);
+    this.newClickData = this.newClickData.bind(this);
+  }
+
+  newClickData() {
+    const now = new Date();
+    const timeString = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}${
+      now.getHours() < 12 ? "am" : "pm"
+    }`;
+    const numClicks = this.state?.clicks?.length + 1 || 1; // The OR condition ensures it works for the initial state
+    return {
+      button: `Click #${numClicks}`,
+      icon: "⭐",
+      textField: `Button click #${numClicks} at ${timeString}`,
     };
   }
 
@@ -20,27 +38,67 @@ class App extends React.Component {
     e.preventDefault();
 
     fetch(this.state.config.apiPrefix + "/api/info", {
-      "method": "GET",
-      "headers": {
-        "accept": "application/json"
-      }
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
     })
-      .then(response => response.json())
-      .then(response => {
+      .then((response) => response.json())
+      .then((response) => {
         this.setState({
-          info: response
-        })
+          info: response,
+        });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }
 
-  componentDidMount() {
+  addPersonRow() {
+    this.setState((prevState) => ({
+      clicks: [...prevState.clicks, this.newClickData()],
+    }));
   }
 
+  componentDidMount() {}
+
   handleChange(changeObject) {
-    this.setState(changeObject)
+    this.setState(changeObject);
+  }
+
+  reverseTextAtIndex(index) {
+    let clicksCopy = [...this.state.clicks];
+    clicksCopy[index].button = clicksCopy[index].button
+      .split("")
+      .reverse()
+      .join("");
+    clicksCopy[index].textField = clicksCopy[index].textField
+      .split("")
+      .reverse()
+      .join("");
+
+    this.setState({
+      clicks: clicksCopy,
+    });
+  }
+
+  reverseRandomPersonText() {
+    if (this.state.clicks.length === 0) return;
+
+    const randomIndex = Math.floor(Math.random() * this.state.clicks.length);
+    let clicksCopy = [...this.state.clicks];
+    clicksCopy[randomIndex].button = clicksCopy[randomIndex].button
+      .split("")
+      .reverse()
+      .join("");
+    clicksCopy[randomIndex].textField = clicksCopy[randomIndex].textField
+      .split("")
+      .reverse()
+      .join("");
+
+    this.setState({
+      clicks: clicksCopy,
+    });
   }
 
   render() {
@@ -48,13 +106,45 @@ class App extends React.Component {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-8">
-            <h1 className="display-4 text-center">Simple API-backed React Page</h1>
+            <h1 className="display-4 text-center">A Village of Wonder</h1>
             <form className="d-flex flex-column">
-              <legend className="text-center">API Info Will Show Up Below.</legend>
-              {this.state.info.application !== "" ? <legend className="text-center">Application: {this.state.info.application}. Version: {this.state.info.version}</legend> : null}
-              <button className="btn btn-primary" type='button' onClick={(e) => this.getApiInfo(e)}>
+              <legend className="text-center">Meet the people</legend>
+              {this.state.info.application !== "" ? (
+                <legend className="text-center">
+                  Application: {this.state.info.application}. Version:{" "}
+                  {this.state.info.version}
+                </legend>
+              ) : null}
+              <button
+                className="btn btn-primary"
+                type="button"
+                onClick={(e) => this.getApiInfo(e)}
+              >
                 Get API Info
               </button>
+              <div className="mt-2">
+                <button
+                  className="btn btn-secondary wide-btn"
+                  type="button"
+                  onClick={this.addPersonRow}
+                >
+                  Add Entry
+                </button>
+                <button
+                  className="btn btn-warning ml-2"
+                  type="button"
+                  onClick={this.reverseRandomPersonText}
+                >
+                  Reverse Random Person Text
+                </button>
+              </div>
+              {this.state.clicks.map((click, index) => (
+                <Person
+                  key={index}
+                  data={click}
+                  handleReverse={() => this.reverseTextAtIndex(index)}
+                />
+              ))}
             </form>
           </div>
         </div>
@@ -63,5 +153,28 @@ class App extends React.Component {
   }
 }
 
-let domContainer = document.querySelector('#App');
+class Person extends React.Component {
+  render() {
+    return (
+      <div className="d-flex align-items-center mt-2">
+        <button
+          className="btn btn-outline-info mr-3 wide-btn"
+          type="button"
+          onClick={this.props.handleReverse}
+        >
+          {this.props.data.button}
+        </button>
+        <span className="icon mr-2">{this.props.data.icon}</span>
+        <input
+          type="text"
+          readOnly
+          className="form-control"
+          value={this.props.data.textField}
+        />
+      </div>
+    );
+  }
+}
+
+let domContainer = document.querySelector("#App");
 ReactDOM.render(<App />, domContainer);
