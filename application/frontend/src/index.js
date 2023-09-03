b0c365fbee34f1938e65f07199540457ad96693e
Gabriel <gamer226+github@gmail.com>
2023-09-03 16:51:01 -0400

    WIP: Make api work with latest changes
    Arg, felt I had to remove the ScoreCalculator module and integrate back into index.js because I couldn't get it to load on the site. Will get back to that later.
    Removed other obsolete files.

--- application/frontend/src/index.js
+++ application/frontend/src/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-import ScoreCalculator from './ScoreCalculator';
-
 const config = {
   apiPrefix: "/dev",
 };
@@ -454,7 +452,7 @@ class HUD extends React.Component {
               <input
                 type="text"
                 readOnly
-                className="form-control"
+                className="form-control" 
                 value={conversation.currentLineIndex}
                 style={{ backgroundColor: conversation.color, color: "#000" }}
               />
@@ -463,6 +461,45 @@ class HUD extends React.Component {
         </div>
       </div>
     );
+  }
+}
+
+class ScoreCalculator {
+  constructor(linesLengths) {
+    this.linesLengths = linesLengths;
+    this.scores = new Array(linesLengths.length).fill(0);
+    this.PERFECT_SCORE = 15;
+    this.OVERESTIMATE_MULTIPLIER = 2;
+    this.UNDERESTIMATE_MULTIPLIER = 3;
+  }
+
+  // Setter method to update scores directly if needed
+  setScoreValues(scores) {
+    if (scores.length !== this.linesLengths.length) {
+      throw new Error("Scores array length doesn't match linesLengths array length");
+    }
+    this.scores = scores;
+  }
+
+  // This method will use the logic from the provided Node class
+  updateScoresForIndex(index, selection) {
+    if (index < 0 || index >= this.scores.length) {
+      throw new Error("Index out of range");
+    }
+
+    const delta = this.linesLengths[index] - selection;
+
+    const deltaMul = delta * ((delta < 0) ? this.OVERESTIMATE_MULTIPLIER : this.UNDERESTIMATE_MULTIPLIER);
+    const score = this.PERFECT_SCORE - Math.abs(deltaMul);
+    this.scores[index] = score;
+    console.log("scores in getScoreForIndex", this.scores);
+
+    return this.scores;
+  }
+
+  // Original method from the React class
+  getTotalScore() {
+    return this.scores.reduce((acc, curr) => acc + curr, 0);
   }
 }
 
