961d6309df72c582c394ed4f3e2a3345e5cae0d3
Gabriel Adauto <gadauto@stanfordalumni.org>
2023-10-09 13:23:16 -0400

    Block users from playing on mobile since UI doesn't support it.

--- application/frontend/src/index.js
+++ application/frontend/src/index.js
@@ -6,9 +6,17 @@ import Village from './components/Village';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Village />
+    <div id="game-container">
+      <Village />
+    </div>
+    <div id="mobile-warning">
+      <p>Hello and thanks for your interest in playing Wonder VillAIge!</p>
+      <p>Alas, I did not make the game interface suitable for mobile devices. 
+        Would you try playing on a computer or tablet?</p>
+      <p>Warm Regards,<br />Gabriel</p>
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
