51e4117c5ecab1a4893f733e16eb49c97ce01c0a
Gabriel Adauto <gadauto@stanfordalumni.org>
2023-10-09 21:00:32 -0400

    Tuned the mobile-block page and added screenshot.

--- application/frontend/src/index.js
+++ application/frontend/src/index.js
@@ -4,16 +4,20 @@ import './css/styles.css';
 import * as serviceWorker from './components/serviceWorker';
 import Village from './components/Village';
 
+import screenshot from './assets/images/screenshot.png';
+
 ReactDOM.render(
   <React.StrictMode>
     <div id="game-container">
       <Village />
     </div>
     <div id="mobile-warning">
-      <p>Hello and thanks for your interest in playing VillAIge of Wonder!</p>
+      <p>Hello and thanks for your interest in playing!</p>
       <p>Alas, I did not make the game interface suitable for mobile devices. 
         Would you try playing on a computer or tablet?</p>
       <p>Warm Regards,<br />Gabriel</p>
+      <img src={screenshot} alt="VillAIge of Wonder screenshot" />
+      
     </div>
   </React.StrictMode>,
   document.getElementById("root")
